4f818c896fa600ca89edd40fbbc906ac7572c6d3
Leo <Leo_Kuo@wistron.com>
2018-03-07 14:32:18 +0800

    Add Log4j2 Controller  =>  127.0.0.1:8080  =>  ERROR Log Add gitignore setting file

--- src/main/java/com/springjpa/SpringJpaPostgreSqlApplication.java
+++ src/main/java/com/springjpa/SpringJpaPostgreSqlApplication.java
@@ -1,7 +1,5 @@
 package com.springjpa;
 
-import org.apache.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -13,7 +11,6 @@ import com.springjpa.services.ICustomerService;
 @SpringBootApplication
 public class SpringJpaPostgreSqlApplication implements CommandLineRunner{
 	
-	private static final org.apache.log4j.Logger logger = LogManager.getLogger(SpringJpaPostgreSqlApplication.class);
 	
 	@Autowired
     ICustomerService iCustomerServ;
@@ -29,11 +26,6 @@ public class SpringJpaPostgreSqlApplication implements CommandLineRunner{
 		Customer cs = new Customer(333, "TestFirstName", "LastName");
 		iCustomerServ.initData(cs);
 		
-		logger.debug("Debugging log");
-        logger.info("Info log");
-        logger.warn("Hey, This is a warning!");
-        logger.error("Oops! We have an Error. OK");
-        logger.fatal("Damn! Fatal error. Please fix me.");
 	}
 	
 }
