669dc15f1e4fee2c71b7972173e556abf4c62a1b
Leo <Leo_Kuo@wistron.com>
2018-03-07 12:03:29 +0800

    add log4j

--- src/main/java/com/springjpa/SpringJpaPostgreSqlApplication.java
+++ src/main/java/com/springjpa/SpringJpaPostgreSqlApplication.java
@@ -1,5 +1,7 @@
 package com.springjpa;
 
+import org.apache.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -10,6 +12,9 @@ import com.springjpa.services.ICustomerService;
 
 @SpringBootApplication
 public class SpringJpaPostgreSqlApplication implements CommandLineRunner{
+	
+	private static final org.apache.log4j.Logger logger = LogManager.getLogger(SpringJpaPostgreSqlApplication.class);
+	
 	@Autowired
     ICustomerService iCustomerServ;
 	
@@ -23,6 +28,12 @@ public class SpringJpaPostgreSqlApplication implements CommandLineRunner{
 		// clear all record if existed before do the tutorial with new data
 		Customer cs = new Customer(333, "TestFirstName", "LastName");
 		iCustomerServ.initData(cs);
+		
+		logger.debug("Debugging log");
+        logger.info("Info log");
+        logger.warn("Hey, This is a warning!");
+        logger.error("Oops! We have an Error. OK");
+        logger.fatal("Damn! Fatal error. Please fix me.");
 	}
 	
 }
