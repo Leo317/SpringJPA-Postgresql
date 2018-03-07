56045f7a925bfb79bedd6ab149737c3c7d119613
Leo <Leo_Kuo@wistron.com>
2018-03-07 15:06:34 +0800

    Add Bean (transactionManager)   and    @EnableAutoConfiguration(exclude = { DataSourceTransactionManagerAutoConfiguration.class})

--- src/main/java/com/springjpa/SpringJpaPostgreSqlApplication.java
+++ src/main/java/com/springjpa/SpringJpaPostgreSqlApplication.java
@@ -5,13 +5,20 @@ import org.hibernate.jpa.HibernateEntityManagerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 
 import com.springjpa.model.Customer;
 import com.springjpa.services.ICustomerService;
 
 @SpringBootApplication
+
+@EnableAutoConfiguration(exclude = { //
+        DataSourceTransactionManagerAutoConfiguration.class})
+
 public class SpringJpaPostgreSqlApplication implements CommandLineRunner{
 	
 	
@@ -35,4 +42,13 @@ public class SpringJpaPostgreSqlApplication implements CommandLineRunner{
 	public SessionFactory sessionFactory(HibernateEntityManagerFactory hemf){    
 	    return hemf.getSessionFactory();    
 	}
+	
+	@Autowired
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager getTransactionManager(SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new HibernateTransactionManager(sessionFactory);
+ 
+        return transactionManager;
+    }
+	
 }
