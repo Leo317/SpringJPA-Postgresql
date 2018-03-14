0401c6a3fb1140c997fbe2b5b0a08f05f611d40b
Leo <Leo_Kuo@wistron.com>
2018-03-14 10:57:08 +0800

    ADD UT

--- src/test/java/com/springjpa/services/CustomerServiceTest.java
+++ src/test/java/com/springjpa/services/CustomerServiceTest.java
@@ -1,6 +1,8 @@
 package com.springjpa.services;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -17,6 +19,7 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.springjpa.dao.CustomerDaoImpl;
+import com.springjpa.exception.CustomerTransactionException;
 import com.springjpa.model.Customer;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -24,7 +27,7 @@ public class CustomerServiceTest {
 	
 	//text Double
 	@Mock
-	private CustomerDaoImpl toDoRepository;
+	private CustomerDaoImpl textDoubleDao;
 	
 	// 要被測試的，也就是SUT(Service層)
 	@InjectMocks
@@ -36,10 +39,72 @@ public class CustomerServiceTest {
 	}
 	
 	@Test
+	public void testCreat() {
+		Customer toDo = new Customer(666, "unit", "test");
+		when(textDoubleDao.find(1L)).thenReturn(toDo);
+		
+		Customer result = textDoubleDao.find(1);
+		
+		assertEquals(0, result.getId());
+		assertEquals(666, result.getPhone());
+	    assertThat(result.getFirstName(), is("unit"));
+	    assertThat(result.getLastName(), is("test"));
+	}
+	
+	@Test
+	public void testFindId() {
+		Customer toDo = new Customer(333, "unit", "test");
+		when(textDoubleDao.find(1L)).thenReturn(toDo);
+		
+		Customer result = toDoService.find(1);
+		
+		assertEquals(0, result.getId());
+		assertEquals(333, result.getPhone());
+		assertEquals("unit", result.getFirstName());
+		assertEquals("test", result.getLastName());
+	}
+	
+	@Test
+	public void testFindAll() {
+		List<Customer> toDoList = new ArrayList<Customer>();
+		toDoList.add(new Customer(1,"Todo Sample 1",""));
+		toDoList.add(new Customer(2,"Todo Sample 2",""));
+		toDoList.add(new Customer(3,"Todo Sample 3",""));
+		when(textDoubleDao.findAll()).thenReturn(toDoList);
+		
+		List<Customer> result = toDoService.findAll();
+		assertEquals(3, result.size());
+	}
+	
+	@Test
+	public void testUpdate() {
+		Customer toDo = new Customer(333, "unit", "test");
+
+		when(textDoubleDao.find(1L)).thenReturn(toDo);
+		Customer result = toDoService.find(1);
+		
+		result.setFirstName("KKK");
+		toDoService.update(result);
+		result = toDoService.find(1);
+		
+		assertThat(result.getId(), is(0L));
+		assertThat(result.getPhone(), is(333L));
+	    assertThat(result.getFirstName(), is("KKK"));
+	    assertThat(result.getLastName(), is("test"));
+	}
+	
+	@Test
+	public void testDelete() {
+		Customer toDo = new Customer(8, "Todo Sample 8", "aaa");
+		toDoService.delete(toDo.getId());
+        verify(textDoubleDao, times(1)).delete(toDo.getId());
+	}
+	
+	@Test
 	public void testGetAll() {
 		Customer toDo = new Customer(666, "unit", "test");
 
-		when(toDoRepository.find(1L)).thenReturn(toDo);
+		when(textDoubleDao.find(1L)).thenReturn(toDo);
 		Customer result = toDoService.find(1);
 		assertEquals(666, result.getPhone());
 		assertEquals("unit", result.getFirstName());
@@ -53,7 +118,7 @@ public class CustomerServiceTest {
 		toDoList.add(new ToDo(1,"Todo Sample 1",true));
 		toDoList.add(new ToDo(2,"Todo Sample 2",true));
 		toDoList.add(new ToDo(3,"Todo Sample 3",false));
-		when(toDoRepository.findAll()).thenReturn(toDoList);
+		when(textDoubleDao.findAll()).thenReturn(toDoList);
 		
 		List<ToDo> result = toDoService.getAllToDo();
 		assertEquals(3, result.size());
@@ -62,7 +127,7 @@ public class CustomerServiceTest {
 	@Test
 	public void testGetToDoById(){
 		ToDo toDo = new ToDo(1,"Todo Sample 1",true);
-		when(toDoRepository.findOne(1L)).thenReturn(toDo);
+		when(textDoubleDao.findOne(1L)).thenReturn(toDo);
 		ToDo result = toDoService.getToDoById(1);
 		assertEquals(1, result.getId());
 		assertEquals("Todo Sample 1", result.getText());
@@ -72,7 +137,7 @@ public class CustomerServiceTest {
 	@Test
 	public void saveToDo(){
 		ToDo toDo = new ToDo(8,"Todo Sample 8",true);
-		when(toDoRepository.save(toDo)).thenReturn(toDo);
+		when(textDoubleDao.save(toDo)).thenReturn(toDo);
 		ToDo result = toDoService.saveToDo(toDo);
 		assertEquals(8, result.getId());
 		assertEquals("Todo Sample 8", result.getText());
@@ -83,7 +148,7 @@ public class CustomerServiceTest {
 	public void removeToDo(){
 		ToDo toDo = new ToDo(8,"Todo Sample 8",true);
 		toDoService.removeToDo(toDo);
-        verify(toDoRepository, times(1)).delete(toDo);
+        verify(textDoubleDao, times(1)).delete(toDo);
 	}
 	*/
 	
